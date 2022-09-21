a818134edb484bf91c7bbfc3fa6b756c3ae645eb
Xifre Font <xifre.font.97@gmail.com>
2022-09-21 18:12:14 +0200

    Tested the route renderer component

--- src/components/RenderRoutes.tsx
+++ src/components/RenderRoutes.tsx
@@ -1,7 +1,11 @@
-import routes from "../routes";
 import { Route, Routes, Navigate } from "react-router-dom";
+import routes from "../routes";
 
-const RenderRoutes = (): JSX.Element => (
+type RenderRoutesProps = {
+  routes: typeof routes;
+};
+
+const RenderRoutes = ({ routes }: RenderRoutesProps): JSX.Element => (
   <Routes>
     {routes.map(({ path, Page, navigate }) => (
       <Route
