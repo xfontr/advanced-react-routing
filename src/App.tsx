ca169208ee0e4867072d6c44a5ac84324eb57c49
Xifre Font <xifre.font.97@gmail.com>
2022-09-22 10:57:03 +0200

    Created a button component and implemented it to log user in or out

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import RenderRoutes from "./components/RenderRoutes";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import routes from "./routes";
+import Button from "./components/Button";
 
-const App = (): JSX.Element => (
-  <div className="app">
-    <Suspense fallback="Loading...">
-      <RenderRoutes routes={routes} isLogged={true} />
-    </Suspense>
-  </div>
-);
+const App = (): JSX.Element => {
+  const [isLogged, setIsLogged] = useState<boolean>(false);
+
+  return (
+    <div className="app">
+      <header>
+        <Button type="button" onClick={() => setIsLogged(!isLogged)}>
+          {isLogged ? "Log out" : "Log in"}
+        </Button>
+      </header>
+
+      <Suspense fallback="Loading...">
+        <RenderRoutes routes={routes} isLogged={isLogged} />
+      </Suspense>
+    </div>
+  );
+};
 
 export default App;
