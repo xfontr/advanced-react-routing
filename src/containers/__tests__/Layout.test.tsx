93ca4cc220409ffae717f41d67d501e3c2a9f6cd
Xifre Font <xifre.font.97@gmail.com>
2022-09-22 17:03:54 +0200

    Tested the layout component

---
+++ src/containers/__tests__/Layout.test.tsx
@@ -0,0 +1,51 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Layout from "../Layout";
+
+describe("Given a Layout component", () => {
+  const text = "Hello";
+  const mockSetter = jest.fn() as React.Dispatch<React.SetStateAction<boolean>>;
+
+  describe("When instantiated with a logged user and a text as a child", () => {
+    test("Then it should render the page layout and said child", () => {
+      render(
+        <Layout isLogged={true} setIsLogged={mockSetter}>
+          {text}
+        </Layout>
+      );
+
+      const logButton = screen.getByRole("button", { name: "Log out" });
+      const childText = screen.getByText(text);
+
+      expect(logButton).toBeInTheDocument();
+      expect(childText).toBeInTheDocument();
+    });
+
+    test("When clicked the log button, it should call the setter function passed as props", async () => {
+      render(
+        <Layout isLogged={true} setIsLogged={mockSetter}>
+          {text}
+        </Layout>
+      );
+
+      const logButton = screen.getByRole("button", { name: "Log out" });
+      await userEvent.click(logButton);
+
+      expect(mockSetter).toHaveBeenCalled();
+    });
+  });
+
+  describe("When instantiated with a not logged user", () => {
+    test("Then the log button should display 'Log in'", () => {
+      render(
+        <Layout isLogged={false} setIsLogged={mockSetter}>
+          {text}
+        </Layout>
+      );
+
+      const logButton = screen.getByRole("button", { name: "Log in" });
+
+      expect(logButton).toBeInTheDocument();
+    });
+  });
+});
